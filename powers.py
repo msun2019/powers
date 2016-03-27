ee07b8637c2381649280e3255cde574e6216ae4f
Michael Sun <msun2019@pingry.org>
2016-03-27 14:14:16 -0400

    Finished, everything working properly

--- powers.py
+++ powers.py
@@ -1,2 +1,15 @@
-def power(base, power)
-	while 
+def power(base, power):
+	if power == 0:
+		answer = 1
+	else:
+		list = []
+		looper = power
+		answer = base
+		while looper != 1:
+			list.append("Value") 
+			looper -= 1
+		for item in list:
+			answer= answer * base
+	print(answer)
+	
+power(4,0)
