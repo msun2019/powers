0f78dcf8bc426b17536ec2511b5ac13aa0cd9e40
Michael Sun <msun2019@pingry.org>
2016-04-25 13:50:05 -0400

    Added test cases

---
+++ powers_test.py
@@ -0,0 +1,6 @@
+import powers 
+
+
+print("The 2nd power of 5 is: {}".format(powers.power(5, 2)))
+print("The 0th power of 360 is: {}".format(powers.power(360, 0)))
+print("The 1st power of 15 is: {}".format(powers.power(15, 1)))
